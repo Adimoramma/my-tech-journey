d6c7c081e04201ad90df49523461055aaef83fa8
Adimoramma <adimoram9@gmail.com>
2026-06-29 21:23:47 +0100

    Day 7: JavaScript variables

---
+++ phase-1-web/week-2/day-7/script.js
@@ -0,0 +1,31 @@
+// Day 7: JavaScript Variables
+
+// Declare variables
+let myName = "Michael";
+let myAge = 27;
+let isStudent = true;
+let myCountry = "Nigeria";
+
+// Copy values between variables
+let first = "One";
+let second = "Two";
+second = first;
+
+console.log("My name is:", myName);
+console.log("I am", myAge, "years old");
+console.log("Am I a student?", isStudent);
+console.log("I live in", myCountry);
+console.log("Second variable is now:", second);
+
+// Basic math
+let sum = 10 + 10;
+let difference = 45 - 33;
+let product = 8 * 10;
+let quotient = 66 / 33;
+let remainder = 10 % 3;
+
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
+console.log("Remainder:", remainder);
